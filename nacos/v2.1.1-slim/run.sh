834b7e8c5d00e6077d2e5971414f5720e781d2e7
AntLiu <liuweizhi.job@foxmail.com>
2024-05-10 00:59:16 +0800

    mongo

--- nacos/v2.1.1-slim/run.sh
+++ nacos/v2.1.1-slim/run.sh
@@ -1,2 +1,8 @@
-docker run --name nacos-quick -e MODE=standalone -p 8848:8848 -d nacos/nacos-server:v2.1.1-slim 
+docker run -d \
+--name nacos-quick \
+-e MODE=standalone \
+-p 8848:8848 \
+-p 9848:9848 \
+-p 9849:9849 \
+nacos/nacos-server:v2.1.1-slim 
 echo 'http://localhost:8848/nacos/'
